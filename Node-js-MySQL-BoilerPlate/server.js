7a2b2dd0ffa3b41e9a41dee7a7722f0e55b5ef41
ericajuarez <erica.juarez5173@gmail.com>
2025-05-18 00:25:31 +0800

    Updated server

--- Node-js-MySQL-BoilerPlate/server.js
+++ Node-js-MySQL-BoilerPlate/server.js
@@ -25,11 +25,11 @@ app.use('/requests', require('./requests/requests.controller'));
 app.use('/api-docs', require('_helper/swagger'));
 
 // Serve Angular static files
-app.use(express.static(path.join(__dirname, '../Angular-10-Boilerplate/dist/angular-signup-verification-boilerplate')));
+app.use(express.static(path.join(__dirname, 'Angular-10-Boilerplate/dist/angular-signup-verification-boilerplate')));
 
 // For any other route, serve Angular's index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Angular-10-Boilerplate/dist/angular-signup-verification-boilerplate/index.html'));
+  res.sendFile(path.join(__dirname, 'Angular-10-Boilerplate/dist/angular-signup-verification-boilerplate/index.html'));
 });
 
 // global error handler
