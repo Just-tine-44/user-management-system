a757842e9c68b7f553ae6c8fd2623d58713dded4
Justine <justineparaiso44@gmail.com>
2025-05-18 15:28:21 +0800

    fix the typo

--- Node-js-MySQL-BoilerPlate/requests/requests.model.js
+++ Node-js-MySQL-BoilerPlate/requests/requests.model.js
@@ -42,7 +42,7 @@ module.exports = (sequelize) => {
             type: DataTypes.INTEGER,
             allowNull: false,
             references: {
-                model: 'Employees',
+                model: 'employees',
                 key: 'id'
             }
         },
