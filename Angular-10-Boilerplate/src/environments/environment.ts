123871c9fc6daa9aaf40e22a4a0258a37997fe80
Vince Bryant Cabunilas <vincebryant42@gmail.com>
2025-05-18 20:29:03 +0800

    Update

--- Angular-10-Boilerplate/src/environments/environment.ts
+++ Angular-10-Boilerplate/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'https://user-management-api.onrender.com'
+    apiUrl: 'https://user-management-system-ir7i.onrender.com'
     
   };
   
