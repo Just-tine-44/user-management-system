2ab88fa72455460f8e4c9dc9a5f273b4ed63d7cb
Vince Bryant Cabunilas <vincebryant42@gmail.com>
2025-05-18 12:09:00 +0800

    updated change

--- Angular-10-Boilerplate/src/environments/environment.prod.ts
+++ Angular-10-Boilerplate/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://user-management-system-b9z4.onrender.com'  
+  apiUrl: 'https://user-management-api.onrender.com'  
 };
--- Node-js-MySQL-BoilerPlate/server.js
+++ Node-js-MySQL-BoilerPlate/server.js
@@ -10,8 +10,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-// allow cors requests from any origin and with credentials
-app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }));
+// Set up CORS - only one CORS configuration needed
+app.use(cors({ 
+  origin: ['https://user-management-frontend.onrender.com'],
+  credentials: true 
+}));
 
 // api routes
 app.use('/accounts', require('./accounts/accounts.controller'));
