ab04ed00b5cd2c1c9938bba46a3d3af2e16f2cab
Vince Bryant Cabunilas <vincebryant42@gmail.com>
2025-05-18 14:24:39 +0800

    Update

--- Angular-10-Boilerplate/render-build.sh
+++ Angular-10-Boilerplate/render-build.sh
@@ -2,7 +2,11 @@
 # exit on error
 set -o errexit
 
+# Remove package-lock to ensure clean install
+rm -f package-lock.json
+
 # Use a Node version compatible with your dependencies
 export NODE_OPTIONS=--max_old_space_size=4096
-npm install --legacy-peer-deps
+export NODE_VERSION=18.17.0
+npm install --legacy-peer-deps --force
 npm run build
